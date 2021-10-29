e375de2c9f0e8b1d00006af26bb183cafe19f23d
rozhko.dmytro <dimarozhko2000@gmail.com>
2021-10-29 15:53:35 +0300

    thread initialization

--- lab2/lab2.go
+++ lab2/lab2.go
@@ -1,5 +1,116 @@
 package lab2
 
-func Lab2() {
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+	"sync"
+	"time"
+)
 
+type stockMoney struct {
+	one        int
+	two        int
+	five       int
+	ten        int
+	twenty     int
+	fifty      int
+	oneHundred int
+}
+
+const (
+	get   = iota
+	post  = iota
+	wait  = iota
+	empty = iota
+)
+
+type mailDTO struct {
+	stock          stockMoney
+	moneyInAccount int
+	transferMoney  int
+	requestId      int
+}
+
+var sessionCounter = 0
+var isATMwork = false
+
+var waittime sync.WaitGroup
+
+var mailbox mailDTO = mailDTO{
+	requestId: empty,
+}
+
+func userThreadA(moneyInAccount int) {
+	sessionCounter++
+	fmt.Println("THREAD A")
+	fmt.Print("If you want to continue Yes/No [y/n]:")
+	var getMoney string
+	fmt.Scan(&getMoney)
+	getMoney = strings.ToLower(getMoney)
+	for getMoney == "y" || getMoney == "yes" {
+		fmt.Printf("You balance in account:\t%d\nHow much money you want to get from ATM?\nInput:\t", moneyInAccount)
+		var getMoneyVal int
+		fmt.Scan(&getMoneyVal)
+		fmt.Println(getMoneyVal)
+		for mailbox.requestId != empty {
+		}
+		// if mailbox.requestId == empty {
+		mailbox.transferMoney = getMoneyVal
+		mailbox.moneyInAccount = moneyInAccount
+		mailbox.requestId = get
+		fmt.Println(mailbox)
+		for mailbox.requestId != post {
+		}
+		fmt.Println(mailbox)
+		moneyInAccount = mailbox.moneyInAccount
+		mailbox.moneyInAccount = 0
+		mailbox.transferMoney = 0
+		mailbox.stock = stockMoney{}
+		mailbox.requestId = empty
+		fmt.Println(mailbox)
+		fmt.Print("If you want to continue Yes/No [y/n]:")
+		fmt.Scan(&getMoney)
+	}
+	sessionCounter--
+	if sessionCounter == 0 {
+		isATMwork = false
+	}
+	waittime.Done()
+}
+
+func atmThreadB(stock stockMoney) {
+	fmt.Println("THREAD B")
+	for isATMwork {
+		if mailbox.requestId == get {
+			mailbox.requestId = wait
+
+			mailbox.requestId = post
+		}
+	}
+	waittime.Done()
+}
+
+func Lab2() {
+	rand.Seed(time.Now().UnixNano())
+	var capacityATM = 1000
+	var ATM stockMoney = stockMoney{
+		one:        rand.Intn(capacityATM),
+		two:        rand.Intn(capacityATM),
+		five:       rand.Intn(capacityATM),
+		ten:        rand.Intn(capacityATM),
+		twenty:     rand.Intn(capacityATM),
+		fifty:      rand.Intn(capacityATM),
+		oneHundred: rand.Intn(capacityATM),
+	}
+
+	fmt.Println(ATM)
+
+	isATMwork = true
+	waittime.Add(2)
+	go userThreadA(4000)
+	go atmThreadB(ATM)
+
+	waittime.Wait()
+	fmt.Println("DONE")
 }
--- main.go
+++ main.go
@@ -7,9 +7,17 @@ package main
 import (
 	"main/lab1"
 	"main/lab2"
+	"os"
 )
 
 func main() {
-	lab1.Lab1()
-	lab2.Lab2()
+	argsWithoutProg := os.Args[1:]
+	for _, arg := range argsWithoutProg {
+		switch arg {
+		case "lab1":
+			lab1.Lab1()
+		case "lab2":
+			lab2.Lab2()
+		}
+	}
 }
