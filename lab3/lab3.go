33e02552ba03e9bb9fe900e71605944f58c9d16f
rozhko.dmytro <dimarozhko2000@gmail.com>
2021-11-01 00:33:49 +0200

    add dataset

--- lab3/lab3.go
+++ lab3/lab3.go
@@ -1,7 +1,34 @@
 package lab3
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func openFileWithCheck(name string, flag int) *os.File {
+	file, err := os.OpenFile(name, flag, 0644)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	return file
+}
 
 func Lab3() {
 	fmt.Println("LAB3")
+	os.MkdirAll("lab3/log", os.ModePerm)
+
+	// cacheLogFile := openFileWithCheck("lab3/log/cache.log", os.O_CREATE|os.O_WRONLY)
+	// mainmemLogFile := openFileWithCheck("lab3/log/mainmem.log", os.O_CREATE|os.O_WRONLY)
+	originDatFile := openFileWithCheck("lab3/data/origin.dat", os.O_RDONLY)
+	mainmemDatFile := openFileWithCheck("lab3/data/mainmem.dat", os.O_RDONLY)
+	cacheDatFile := openFileWithCheck("lab3/data/cache.dat", os.O_RDONLY)
+
+	buff, _ := ioutil.ReadAll(originDatFile)
+	fmt.Println(buff)
+	buff, _ = ioutil.ReadAll(mainmemDatFile)
+	fmt.Println(buff)
+	buff, _ = ioutil.ReadAll(cacheDatFile)
+	fmt.Println(buff)
+
 }
