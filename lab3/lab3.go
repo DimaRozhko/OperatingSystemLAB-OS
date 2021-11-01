09ae6a06a90d028cdc51bd0aaa80ca1bd7c3c517
rozhko.dmytro <dimarozhko2000@gmail.com>
2021-11-02 01:04:29 +0200

    undo revert

--- lab3/lab3.go
+++ lab3/lab3.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func openFileWithCheck(name string, flag int) *os.File {
@@ -13,11 +15,9 @@ func openFileWithCheck(name string, flag int) *os.File {
 	}
 	return file
 }
-
 func Lab3() {
 	fmt.Println("LAB3")
 	os.MkdirAll("lab3/log", os.ModePerm)
-
 	// cacheLogFile := openFileWithCheck("lab3/log/cache.log", os.O_CREATE|os.O_WRONLY)
 	// mainmemLogFile := openFileWithCheck("lab3/log/mainmem.log", os.O_CREATE|os.O_WRONLY)
 	originDatFile := openFileWithCheck("lab3/data/origin.dat", os.O_RDONLY)
@@ -26,9 +26,27 @@ func Lab3() {
 
 	buff, _ := ioutil.ReadAll(originDatFile)
 	fmt.Println(buff)
+	originData := strings.Fields(string(buff))
+	fmt.Println(originData)
 	buff, _ = ioutil.ReadAll(mainmemDatFile)
 	fmt.Println(buff)
+	mainmemDat := strings.Fields(string(buff))
+	fmt.Println(mainmemDat)
+	var kayToMainMemMap []int
+	mainMemMap := make(map[int]string)
+	for i, tok := range mainmemDat {
+		if i%2 == 0 {
+			num, _ := strconv.ParseInt(tok, 2, 64)
+			kayToMainMemMap = append(kayToMainMemMap, int(num))
+		} else {
+			mainMemMap[kayToMainMemMap[len(kayToMainMemMap)-1]] = tok
+		}
+	}
+	fmt.Println(kayToMainMemMap)
+	fmt.Println(mainMemMap)
 	buff, _ = ioutil.ReadAll(cacheDatFile)
 	fmt.Println(buff)
+	cacheDat := strings.Fields(string(buff))
+	fmt.Println(cacheDat)
 
 }
