efeb10db030d0358a36cd94e81a25a6132f61768
rozhko.dmytro <dimarozhko2000@gmail.com>
2021-11-07 22:23:28 +0200

    LAB3 DONE

--- lab3/lab3.go
+++ lab3/lab3.go
@@ -57,9 +57,6 @@ func queryRunner(originData []string) {
 
 func Lab3() {
 	fmt.Println("LAB3")
-	os.MkdirAll("lab3/log", os.ModePerm)
-	// cacheLogFile := openFileWithCheck("lab3/log/cache.log", os.O_CREATE|os.O_WRONLY)
-	// mainmemLogFile := openFileWithCheck("lab3/log/mainmem.log", os.O_CREATE|os.O_WRONLY)
 	originDatFile := openFileWithCheck("lab3/data/origin.dat", os.O_RDONLY)
 	mainmemDatFile := openFileWithCheck("lab3/data/mainmem.dat", os.O_RDONLY)
 	cacheDatFile := openFileWithCheck("lab3/data/cache.dat", os.O_RDONLY)
