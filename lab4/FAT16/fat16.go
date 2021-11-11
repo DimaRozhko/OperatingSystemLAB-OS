f187fd2760b0d64a53ccf1152666ba79c63e69f4
rozhko.dmytro <dimarozhko2000@gmail.com>
2021-11-11 02:33:42 +0200

    init FAT16 table

--- lab4/FAT16/fat16.go
+++ lab4/FAT16/fat16.go
@@ -1,5 +1,33 @@
 package fat16
 
+import (
+	"fmt"
+)
+
+type clusterCode struct {
+	word byte
+}
+
+const (
+	clusterLength      = 0xFF + 1
+	usageClusterLength = 0x0F
+	firstUsageCluster  = 0x06 // only for first row in the table
+)
+
+var fat16Table [clusterLength][clusterLength]clusterCode
+
+// var fat16ReserveTable [clusterLength][clusterLength]clusterCode
+
 func TableCreator() {
+	// Set necessary file to run OS
+	fat16Table[0x00][0x01] = clusterCode{word: 0xff}
+	fat16Table[0x00][0x02] = clusterCode{word: 0x03}
+	fat16Table[0x00][0x03] = clusterCode{word: 0x04}
+	fat16Table[0x00][0x04] = clusterCode{word: 0x05}
+	fat16Table[0x00][0x05] = clusterCode{word: 0xff}
+	fmt.Println(fat16Table[0])
+}
+
+func AddFileToFAT16Table() {
 
 }
--- lab4/lab4.go
+++ lab4/lab4.go
@@ -1,7 +1,21 @@
 package lab4
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	fat16 "main/lab4/FAT16"
+	"os"
+	"strings"
+)
 
 func Lab4() {
 	fmt.Println("LAB4")
+	file, err := os.OpenFile("lab4/data/fileinfo.dat", os.O_RDONLY, 0644)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	buff, _ := ioutil.ReadAll(file)
+	fat16.TableCreator()
+	s := strings.Split(string(buff), "\n")
+	fmt.Println(s[0])
 }
